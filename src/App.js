e94bea825cb90664144888bfe526c60f55317d6b
Waleed <waleed.mukhtar1999@gmail.com>
2024-06-12 21:16:17 +0500

    links removed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
+import File from "./components/File/File";
 import {
   BrowserRouter as Router,
   Route,
@@ -36,10 +37,11 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
+          {/* <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
+          <Route path="/resume" element={<Resume />} /> */}
           <Route path="*" element={<Navigate to="/"/>} />
+          {/* <Route path="app-ads.txt" element ={<File/>}/> */}
         </Routes>
         <Footer />
       </div>
---
+++ src/components/File/File.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function File() {
+
+    useEffect(()=>{
+
+            window.location.href = "https://gdc237212.app-ads-txt.com/";
+        
+    },[])
+  return (
+    <div>File</div>
+  )
+}
+
+export default File
